1d80703acaa02c58457e41526363e757684c20f6
root <root@ik1-316-18130.vs.sakura.ne.jp>
2015-12-25 01:28:49 +0900

    add zshrc install-script

---
+++ zsh/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/.zshrc.alias ~/.zshrc.alias
+ln -s ~/dotfiles/zsh/.zshrc.history ~/.zshrc.history
+ln -s ~/dotfiles/zsh/.zshrc.color ~/.zshrc.color
+ln -s ~/dotfiles/zsh/.zshrc.prompt ~/.zshrc.prompt
