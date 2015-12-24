4f841f64554198d916971bf80f0d89e5d23d2318
kash <krswshnt@gmail.com>
2015-12-25 01:53:10 +0900

    check exist in zsh-install-script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+[ -e ~/.zshrc ] && rm ~/.zshrc
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+[ -e ~/.zshrc.alias ] && rm ~/.zshrc.alias
 ln -s ~/dotfiles/zsh/.zshrc.alias ~/.zshrc.alias
+[ -e ~/.zshrc.history ] && rm ~/.zshrc.history
 ln -s ~/dotfiles/zsh/.zshrc.history ~/.zshrc.history
+[ -e ~/.zshrc.color ] && rm ~/.zshrc.color
 ln -s ~/dotfiles/zsh/.zshrc.color ~/.zshrc.color
+[ -e ~/.zshrc.prompt ] && rm ~/.zshrc.prompt
 ln -s ~/dotfiles/zsh/.zshrc.prompt ~/.zshrc.prompt
