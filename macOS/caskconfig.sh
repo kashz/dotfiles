fb51d04390bd6a65ed4009a6a9124d85ffeb5c8b
Shunta KARASAWA <krswshnt@gmail.com>
2016-12-22 20:49:35 +0900

    add cask config

---
+++ macOS/caskconfig.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+#
+# General
+brew cask install google-japanese-ime
+brew cask install appcleaner
+brew cask install bettertouchtool
+
+# Browser
+brew cask install google-chrome
+brew cask install vivaldi
+brew cask install opera
+brew cask install firefox
+
+# Develop
+brew cask install atom
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install sourcetree
+brew cask install tunnelblick
+brew cask install filezilla
+brew cask install virtualbox
+
+# Social
+brew cask install slack
+brew cask install skype
+brew cask install evernote
+brew cask install dropbox
+brew cask install vlc
+brew cask install utorrent
+
+# Other
+brew cask install keepassx
+brew cask install skitch
+brew cask install flux
+brew cask install karabiner
